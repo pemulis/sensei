59fdef5b1337524680bb5eedf5b43011c8861305
John Shutt <pemulis@users.noreply.github.com>
2024-08-08 15:08:00 -0700

    try to increase volume of audio playback to max
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -118,6 +118,14 @@ const Home = () => {
     privyLogin();
   }, [authenticated, wallets]);
 
+  useEffect(() => {
+    // Set the volume of each audio element to 1.0 after the component has been rendered
+    const audioElements = threadContainerRef.current.querySelectorAll('audio');
+    audioElements.forEach(audio => {
+      audio.volume = 1.0; // Set volume to maximum
+    });
+  }, [messages]); // Re-run this effect whenever messages change  
+
   const handleStartRecording = async () => {
     try {
       // Check the microphone permission status
